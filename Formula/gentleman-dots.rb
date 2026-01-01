e0da5436f206aa05497c3a01866106a284587f70
Alan Buscaglia <gentlemanprogramming@gmail.com>
2026-01-01 20:49:06 +0100

    chore: bump to v2.2.0 - fix Ubuntu install, remove bash script

--- Formula/gentleman-dots.rb
+++ Formula/gentleman-dots.rb
@@ -1,10 +1,10 @@
 class GentlemanDots < Formula
   desc "TUI installer for Gentleman.Dots terminal environment"
   homepage "https://github.com/Gentleman-Programming/Gentleman.Dots"
-  url "https://github.com/Gentleman-Programming/Gentleman.Dots/archive/refs/tags/v2.1.3.tar.gz"
-  sha256 "d576713cf260b74040dcbf8a3cb84d53276c52b8e7a82200fa989cce68e2e393"
+  url "https://github.com/Gentleman-Programming/Gentleman.Dots/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "8a56fd3f64dd8a984bf269d85ba8de33aab33ef44a93a7feb17ade4785d54a75"
   license "MIT"
-  version "2.1.3"
+  version "2.2.0"
 
   depends_on "go" => :build
 
