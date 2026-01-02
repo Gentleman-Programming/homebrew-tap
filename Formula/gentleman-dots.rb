824ad4717f2fa286b928288cfc564c57667ef861
Alan Buscaglia <gentlemanprogramming@gmail.com>
2026-01-02 16:04:51 +0100

    fix: reorder version field for better brew resolution

--- Formula/gentleman-dots.rb
+++ Formula/gentleman-dots.rb
@@ -1,10 +1,10 @@
 class GentlemanDots < Formula
   desc "TUI installer for Gentleman.Dots terminal environment"
   homepage "https://github.com/Gentleman-Programming/Gentleman.Dots"
-  url "https://github.com/Gentleman-Programming/Gentleman.Dots/archive/refs/tags/v2.4.3.tar.gz"
+  version "2.4.3"
+  url "https://github.com/Gentleman-Programming/Gentleman.Dots/archive/refs/tags/v#{version}.tar.gz"
   sha256 "2645e2070db24e71514bc268c98cf22bbe0c183e8f5b1fccc7df413ec5bb6178"
   license "MIT"
-  version "2.4.3"
 
   depends_on "go" => :build
 
