b4de3535832a1c47d990036d0b7cc106521fe5b1
Alan Buscaglia <gentlemanprogramming@gmail.com>
2026-01-02 14:55:31 +0100

    fix: bump to v2.4.1 - create .config directory before copying configs

--- Formula/gentleman-dots.rb
+++ Formula/gentleman-dots.rb
@@ -1,10 +1,10 @@
 class GentlemanDots < Formula
   desc "TUI installer for Gentleman.Dots terminal environment"
   homepage "https://github.com/Gentleman-Programming/Gentleman.Dots"
-  url "https://github.com/Gentleman-Programming/Gentleman.Dots/archive/refs/tags/v2.4.0.tar.gz"
-  sha256 "51482e1bf968a64db84d6d17a00794affbf4fc02be463e78728a56388a373119"
+  url "https://github.com/Gentleman-Programming/Gentleman.Dots/archive/refs/tags/v2.4.1.tar.gz"
+  sha256 "66edd4fa4303b8a119417272bf4a8867ba591fb896ea1d14843e36d0695dfd25"
   license "MIT"
-  version "2.4.0"
+  version "2.4.1"
 
   depends_on "go" => :build
 
