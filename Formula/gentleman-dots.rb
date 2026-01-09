c3b5a9eecc78cdc06c9de0def59350f47d59e73a
Alan Buscaglia <gentlemanprogramming@gmail.com>
2026-01-10 00:02:35 +0100

    feat: bump gentleman-dots to v2.5.0

--- Formula/gentleman-dots.rb
+++ Formula/gentleman-dots.rb
@@ -1,9 +1,9 @@
 class GentlemanDots < Formula
   desc "TUI installer for Gentleman.Dots terminal environment"
   homepage "https://github.com/Gentleman-Programming/Gentleman.Dots"
-  version "2.4.4"
+  version "2.5.0"
   url "https://github.com/Gentleman-Programming/Gentleman.Dots/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "ab96bfbf99532157b7da90df89cccb5b79a96bc52f6d1ad54e8ddea8722a1cc9"
+  sha256 "348cc3289893f38a1f5508c1882845abed6ab44d6dee9ad46b7035f3981bae97"
   license "MIT"
 
   depends_on "go" => :build
