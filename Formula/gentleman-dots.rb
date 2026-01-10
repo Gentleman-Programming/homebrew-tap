4d33230fcd6ea83fe35d450ffe682b7c2bbf2230
Alan Buscaglia <gentlemanprogramming@gmail.com>
2026-01-10 02:18:23 +0100

    chore: bump version to v2.5.2
    - Fix shell configs patch based on WM and nvim choices
    - Remove WM-related lines when user selects 'none' (fixes Gentleman.Dots#106, #103)
    - Replace tmux with zellij when user selects 'zellij'
    - Wrap fzf with existence check when nvim not installed
    - Use pre-built binaries instead of building from source

--- Formula/gentleman-dots.rb
+++ Formula/gentleman-dots.rb
@@ -1,20 +1,38 @@
 class GentlemanDots < Formula
-  desc "TUI installer for Gentleman.Dots terminal environment"
+  desc "Interactive TUI installer for Gentleman.Dots development environment"
   homepage "https://github.com/Gentleman-Programming/Gentleman.Dots"
-  version "2.5.1"
-  url "https://github.com/Gentleman-Programming/Gentleman.Dots/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "45ba138840880e3affc13533fa29eae7b16f3c0b84067a5844e8aec0a918252f"
+  version "2.5.2"
   license "MIT"
 
-  depends_on "go" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-darwin-arm64"
+      sha256 "15b62ff9e62ef9db449c4182cad6132e93b42f35d15c9758ab37a14f748ba8a9"
+    end
+    on_intel do
+      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-darwin-amd64"
+      sha256 "9fbe75cb51821d49d9800e0f1876aee4667453a8df8d1e95bf2c736416a96ad9"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/Gentleman-Programming/Gentleman.Dots/releases/download/v#{version}/gentleman-installer-linux-amd64"
+      sha256 "9e9041f32db1a9405cb9be5f89c80575099904acf8a6cba387c938a753fe05cc"
+    end
+  end
 
   def install
-    cd "installer" do
-      system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=#{version}"), "-o", bin/"gentleman.dots", "./cmd/gentleman-installer"
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "gentleman-installer-darwin-arm64" => "gentleman-dots"
+    elsif OS.mac? && Hardware::CPU.intel?
+      bin.install "gentleman-installer-darwin-amd64" => "gentleman-dots"
+    elsif OS.linux? && Hardware::CPU.intel?
+      bin.install "gentleman-installer-linux-amd64" => "gentleman-dots"
     end
   end
 
   test do
-    assert_match "gentleman.dots v#{version}", shell_output("#{bin}/gentleman.dots --version")
+    system "#{bin}/gentleman-dots", "--help"
   end
 end
